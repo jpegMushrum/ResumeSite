ada098b61fec2e5505ea12291bfaaebbe45457ab
jpegMushrum <artem070705@yandex.ru>
2023-07-13 22:53:21 +0300

    Dropping text done

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,14 +4,21 @@ import DropText from './DropText.js'
 
 function App() {
     const abtme = "Male, 18 y.o., I'm dreaming about gamedev. Now learning JS/TS, React, TON."
-    const ach = ""
+    const ach = (
+        <ul>
+            <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340065436392/color.pdf"> Cryptography olympiad (Mathematics + Cryptography)</a></li>
+            <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340230856102/color.pdf"> Open olympiad of schoolchild (Informatics)</a></li>
+            <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340180667460/color.pdf"> "Бельчонок" olympiad (Informatics)</a></li>
+            <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2022/by-code/290403264600/color.pdf"> "Бельчонок" olympiad (Mathematics)</a></li>
+        </ul>
+    );
 
     return (
         <div>
             <Title />
-            <DropText title = 'About me' body = {abtme}/>
-            <DropText title = 'Achievments' body = {ach}/>
-            <h3> Nice to see you there!</h3>
+            <DropText title = 'About me' body = {abtme} idButton = {1}/>
+            <DropText title = 'Achievments' body = {ach} idButton = {2}/>
+            <h3 id = "byeing"> Nice to see you there!</h3>
         </div>
     );
 }
--- resume/src/DropText.js
+++ resume/src/DropText.js
@@ -1,26 +1,37 @@
 import React from 'react'
-
-let isOpen = false;
-const OpenClose = () => {
-    if (isOpen) {
-        document.getElementById('hui').innerHTML = "My new text!";
-    }
-    else {
-        $("#hui").innerHTML = "hui";
-    }
-}
+//import $ from './other/jquery-3.7.0.js'
+import $ from 'jquery';
 
 function DropText(props) {
     const Title = props.title;
     const Body = props.body;
+    const id = props.idButton;
+    const idBody = `b-${id}`
+    let isOpen = false;
     
+    const OpenClose = () => {
+        if (isOpen) {
+            $("#" + id)[0].innerHTML = "<i class=\"fa-sharp fa-solid fa-chevron-left\"/>";
+            $("#" + idBody).toggle("fast");
+            isOpen = !isOpen;
+        }
+        else {
+            
+            $("#" + id)[0].innerHTML = "<i class=\"fa-sharp fa-solid fa-chevron-down\"/>";
+            $("#" + idBody).toggle("fast");
+            isOpen = !isOpen;
+        }
+    }
+
+    $("#" + idBody)[0].style.display = 'none';
+
     return (
         <div>
             <div class = "OpenCloseButton">
                 <h2 class = "DropTitle"> {Title}</h2>
-                <button id = "hui" onClick = {OpenClose()}> &lt; </button>
+                <button id = {id} onClick = {OpenClose}> <i class="fa-sharp fa-solid fa-chevron-left"/></button>
             </div>
-            <h3 class = "DropBody"> {Body}</h3>
+            <h3 id = {idBody} class = "DropBody"> {Body}</h3>
         </div>
     );
 }
