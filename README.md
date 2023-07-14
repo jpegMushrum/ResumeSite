69e9276e676a1eeb441270c083c1d79e06fd8b6e
jpegMushrum <artem070705@yandex.ru>
2023-07-14 20:22:00 +0300

    Complemented information, added CF API

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ It's a code of my resume site. It's experemental + training, nothing serious.
 ## Feutures 
 - Blocks with animated dropping containers.
 - Automaticly changing my age
+- Codforces API is used to make information actiual.
--- resume/src/App.js
+++ resume/src/App.js
@@ -11,6 +11,24 @@ function getAge (date) {
     return result;
 }
 
+function getCodeForcesInfo () {
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://codeforces.com/api/user.info?handles=jpegMushrum&lang=en", false);
+    request.send();
+    const response = request.response;
+
+    let responseJson = JSON.parse(response);
+    responseJson = responseJson['result'][0];
+    
+    const result = {
+        handle: responseJson['handle'],
+        rating: responseJson['rating'],
+        rank: responseJson['rank']
+    };
+   
+    return result;
+}
+
 function App() {
     let today = new Date();
     today = {
@@ -19,25 +37,56 @@ function App() {
         year: today.getFullYear()
     };
     const age = getAge(today);
-    console.log(age);
+    
+    const codeForcesInfo = getCodeForcesInfo(); 
 
-    const abtme = `Male, ${age} y.o., I'm dreaming about gamedev. Now learning JS/TS, React, TON.`
-    const ach = (
+    const aboutMe = `Male, ${age} y.o., I'm dreaming about gamedev. Now learning JS/TS, React, TON.`
+    const achievments = (
         <>
-            <ul class = "AchievmentsList">
-                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340065436392/color.pdf"> Cryptography olympiad (Mathematics + Cryptography)</a></li>
-                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340230856102/color.pdf"> Open olympiad of schoolchild (Informatics)</a></li>
-                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340180667460/color.pdf"> "Бельчонок" olympiad (Informatics)</a></li>
-                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2022/by-code/290403264600/color.pdf"> "Бельчонок" olympiad (Mathematics)</a></li>
+            <ul>
+                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340065436392/color.pdf"> Cryptography olympiad (Mathematics + Cryptography).</a></li>
+                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340230856102/color.pdf"> Open olympiad of schoolchildren (Informatics).</a></li>
+                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2023/by-code/340180667460/color.pdf"> "Бельчонок" olympiad (Informatics).</a></li>
+                <li><a href = "https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-2022/by-code/290403264600/color.pdf"> "Бельчонок" olympiad (Mathematics).</a></li>
             </ul>
         </>
     );
 
+    const skills = (
+        <>
+            <ul>
+                <li>
+                    Porgramming languages: C# (Windows Forms, Unity), C++ (Algorithms and data structures | Olympiad programming), Python (Parsers using API), 
+                    JS|TS (React), a few of assembler (nasm).
+                </li>
+                <li>
+                    Hard skills: Programming: Simple UI, API Queries, Algorithms.
+                </li>
+                <li>Languages: Russian (Native), English (B2).</li>
+            </ul>
+        </>
+    );
+    
+    const git = (
+        <>
+            <p> My profile on GitHub: <a href="https://github.com/jpegMushrum">jpegMushrum</a>. </p>
+            <div> CodeForces profile info: 
+                <ul>
+                    <li>Handle and link: <a href = "https://codeforces.com/profile/jpegMushrum">jpegMushrum</a>.</li>
+                    <li>Rank: {codeForcesInfo.rank}. </li>
+                    <li>Rating: {codeForcesInfo.rating}.</li>
+                </ul>
+            </div>
+        </>
+    );
+    
     return (
         <div>
             <Title />
-            <DropText title = 'About me' body = {abtme} idButton = {1}/>
-            <DropText title = 'Achievments' body = {ach} idButton = {2}/>
+            <DropText title = 'About me' body = {aboutMe} idButton = {1}/>
+            <DropText title = 'Achievments' body = {achievments} idButton = {2}/>
+            <DropText title = 'Skills' body = {skills} idButton = {3}/>
+            <DropText title = 'Git and CodeForces' body = {git} idButton = {4}/>
             <h3 id = "byeing"> Nice to see you here!</h3>
         </div>
     );
